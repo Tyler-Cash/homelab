ec27c1fda692c0c6606f592848d447e4d3feda9d
Tyler <12643375+Tyler-Cash@users.noreply.github.com>
2023-03-07 00:06:28 +1100

    Fix CoreDNS adblocking

--- kubernetes/helm/networking/coredns/manifests/scripts/download_blacklists.sh
+++ kubernetes/helm/networking/coredns/manifests/scripts/download_blacklists.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # Taken from https://news.ycombinator.com/item?id=21238213
 HOSTS_FILE="/tmp/hosts.blacklist"
 destination="/blacklist"
@@ -9,26 +10,26 @@ download() {
 echo "download($1)"
 curl \
     --location --max-redirs 3 \
-    --max-time 20 --retry 3 --retry-delay 0 --retry-max-time 60 \
+    --max-time 600 --retry 3 --retry-delay 0 --retry-max-time 1000 \
     "$1" > "$(mktemp "${HOSTS_FILES}"/XXXXXX)"
 }
 
 download "https://big.oisd.nl/domains"
 
 cat "${HOSTS_FILES}"/* | \
-sed \
--e 's/0.0.0.0//g' \
--e 's/127.0.0.1//g' \
--e '/255.255.255.255/d' \
--e '/::/d' \
--e '/#/d' \
--e 's/ //g' \
--e 's/  //g' \
--e '/^$/d' \
--e 's/^/0.0.0.0 /g' | \
-awk '!a[$0]++' | \
-sed \
--e '/tylercash.dev/d' > "${HOSTS_FILE}"
+    sed \
+        -e 's/0.0.0.0//g' \
+        -e 's/127.0.0.1//g' \
+        -e '/255.255.255.255/d' \
+        -e '/::/d' \
+        -e '/#/d' \
+        -e 's/ //g' \
+        -e 's/  //g' \
+        -e '/^$/d' \
+        -e 's/^/0.0.0.0 /g' | \
+            awk '!a[$0]++' | \
+                sed \
+                    -e '/tylercash.dev/d' > "${HOSTS_FILE}"
 
 rm -rf "${HOSTS_FILES}"
 
